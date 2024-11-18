250d60fc95836e3cc1649f771daed6ee774d7303
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-11-18 16:45:18 +0100

    create lesson class service and createLesson method

---
+++ src/lesson/lesson.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Lesson } from './lesson.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
+  ) {}
+
+  async createLesson(name, startDate, endDate): Promise<Lesson> {
+    const lesson = this.lessonRepository.create({
+      name,
+      startDate,
+      endDate,
+    });
+
+    return this.lessonRepository.save(lesson);
+  }
+}
