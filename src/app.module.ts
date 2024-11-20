609cf100af79566b527ef4c9e5e2502c661fed16
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-11-20 09:33:08 +0100

    defining student entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { LessonModule } from './lesson/lesson.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson/lesson.entity';
 import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { StudentModule } from './student/student.module';
       url: 'mongodb://localhost/school',
       synchronize: true,
       useUnifiedTopology: true,
-      entities: [Lesson],
+      entities: [Lesson, Student],
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
