5c81031f73081fe6a4d5b785e8f7d80522bda51a
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-11-20 09:50:13 +0100

    created student mutation

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Student } from './student.entity';
+import { Repository } from 'typeorm';
+import { CreateStudentInput } from './studen-input.dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
+
+  async createStudent(
+    createStudentInput: CreateStudentInput,
+  ): Promise<Student> {
+    const { firstName, lastName } = createStudentInput;
+    const student = this.studentRepository.create({
+      id: randomUUID(),
+      firstName,
+      lastName,
+    });
+    return this.studentRepository.save(student);
+  }
+}
