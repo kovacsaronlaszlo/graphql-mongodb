231c944bae175ba48f7d8308d15120a07a5faafe
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-11-20 09:53:17 +0100

    get all students

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { StudentType } from './student.type';
 import { CreateStudentInput } from './studen-input.dto';
 import { StudentService } from './student.service';
@@ -6,6 +6,11 @@ import { StudentService } from './student.service';
 @Resolver((of) => StudentType)
 export class StudentResolver {
   constructor(private studentService: StudentService) {}
+
+  @Query((returns) => [StudentType])
+  async students() {
+    return this.studentService.getStudents();
+  }
 
   @Mutation((returns) => StudentType)
   createStudent(
